c621534d6f55e11e8d326691930aa229971ee274
zwang <zachary.wang@gmail.com>
2019-04-18 12:57:16 -0700

    buggy navbar fix

--- js/script.js
+++ js/script.js
@@ -4,15 +4,19 @@ $(document).ready(function() {
 
     var navbar = document.getElementById("navigation");
 
+    var about = document.getElementById("about");
+
     // Get the offset position of the navbar
-    var fixed = navbar.offsetTop;
+    var sticky = navbar.offsetTop;
 
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function navCheck() {
-        if (window.pageYOffset >= fixed) {
-            navbar.classList.add("fixed-top");
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+            about.classList.add("pad");
         } else {
-            navbar.classList.remove("fixed-top");
+            navbar.classList.remove("sticky");
+            about.classList.remove("pad");
         }
     }
 
